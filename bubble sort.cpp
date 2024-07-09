de25cee932ca891ff5562c2227d6ec008c89aa9b
Azhar Hamim <azharhamim80@gmail.com>
2024-07-09 20:48:06 +0600

    ease enter the commit message for your changes. Lines starting

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-int arr[]={5,4,3,2,1};
+int arr[]={6,4,3,2,1};
     for(int i =0;i<5-1;i++){
         for(int j=0;j<5-i-1;j++){
             if(arr[j]>arr[j+1]){
